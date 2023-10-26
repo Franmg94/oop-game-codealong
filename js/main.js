e3f4859b944a96a42b66b1fe7bc6e8507723dae8
Fran Martinez <pmg.webdev@gmail.com>
2023-10-26 19:34:16 +0200

    player on the middle, max range left-right, obstacles appear randomly

--- js/main.js
+++ js/main.js
@@ -1,101 +1,107 @@
+console.log(window.innerWidth)
+
 class Player {
     constructor() {
-// initialize properties
-        this.positionX = 50;
+        // initialize properties
+        this.positionX = 40;
         this.positionY = 0;
         this.height = 10;
-        this.width = 20;
-// om manipulation to reflect initial values (size, position)
-// create the element
+        this.width = 12;
+
+        // dom manipulation to reflect initial values (size, position)
         this.playerElm = document.getElementById("player");
-// size
         this.playerElm.style.width = this.width + "vw";
         this.playerElm.style.height = this.height + "vh";
-// position
         this.playerElm.style.left = this.positionX + "vw";
         this.playerElm.style.bottom = this.positionY + "vh";
     }
     moveLeft() {
-        this.positionX--;
-        this.playerElm.style.left = this.positionX + "vw";
+        if(this.positionX > 0){
+            this.positionX--;
+            this.playerElm.style.left = this.positionX + "vw";
+        }
     }
     moveRight() {
-        this.positionX++;
-        this.playerElm.style.left = this.positionX + "vw";
+        if (this.positionX < 80) {
+            this.positionX++;
+            this.playerElm.style.left = this.positionX + "vw";
+            console.log(this.positionX);
+        }
+
     }
 }
 
 
 class Obstacle {
     constructor(){
-        this.positionX = 50;
+        const randomX = Math.floor(Math.random() * 81) ;
+
+        this.positionX = randomX;
         this.positionY = 100;
         this.height = 10;
         this.width = 30;
         this.obstacleElm = null;
 
         this.createDomElement();
-        
     }
     createDomElement(){
-         // step1: create the element
-         this.obstacleElm = document.createElement("div");  // important this
+        // step1: create the element
+        this.obstacleElm = document.createElement("div");
 
-         // step2: add content or modify
-         this.obstacleElm.classList.add("obstacle");
-         this.obstacleElm.style.width = this.width + 'vw';
-         this.obstacleElm.style.height = this.height + 'vh';
-         this.obstacleElm.style.left = this.positionX + 'vw';
-         this.obstacleElm.style.bottom = this.positionY + 'vh';
-         
-         // step3: append to the dom: `parentElm.appendChild()
-         const parentElm = document.getElementById("board");
-         parentElm.appendChild(this.obstacleElm);
+        // step2: add content or modify
+        this.obstacleElm.classList.add("obstacle");
+        this.obstacleElm.style.width = this.width + "vw";
+        this.obstacleElm.style.height = this.height + "vh";
+        this.obstacleElm.style.left = this.positionX + "vw";
+        this.obstacleElm.style.bottom = this.positionY + "vh";
+        
+        // step3: append to the dom: `parentElm.appendChild()`
+        const parentElm = document.getElementById("board");
+        parentElm.appendChild(this.obstacleElm);
     }
-
     moveDown(){
         this.positionY--;
-        this.obstacleElm.style.bottom = this.positionY + 'vh';
-        console.log('moving down');
-        
+        this.obstacleElm.style.bottom = this.positionY + "vh";
     }
 }
 
 
-// display the class on the browser
+
+
 const player = new Player();
 
-const obstaclesArr = [];   // will store instances of the class Obstacle, important 
+const obstaclesArr = []; // will store instances of the class Obstacle
 
-// setInterval(() =>{
-//     ob1.moveDown();
-// }, 1000)
 
-// create obstacles 
+// create obstacles
 setInterval(() => {
     const newObstacle = new Obstacle();
     obstaclesArr.push(newObstacle);
-    console.log(newObstacle);
-    console.log(obstaclesArr);
-}, 2000)
+}, 3000);
 
-//move obstacles (ex. every  XXXms, move all the obstacles that we have in the array)
 
+
+// update obstacles
 setInterval(() => {
-    obstaclesArr.forEach((obstacleInstance) => {
+    obstaclesArr.forEach( (obstacleInstance) => {
+        // move
         obstacleInstance.moveDown();
-    })
+
+        // detect collision rectangular
+        if (
+            player.positionX < obstacleInstance.positionX + obstacleInstance.width &&
+            player.positionX + player.width > obstacleInstance.positionX &&
+            player.positionY < obstacleInstance.positionY + obstacleInstance.height &&
+            player.positionY + player.height > obstacleInstance.positionY
+          ){
+            // collision detected 
+            console.log('game over!');
+            location.href = "./gameover.html";
+          }
+    });
 }, 50);
 
 
-ob1.moveDown();
-ob1.moveDown();
-ob1.moveDown();
-ob1.moveDown();
-ob1.moveDown();
-
-
-// Detects if we pressed a key
 document.addEventListener("keydown", (e) => {
     switch (e.code) {
         case "ArrowLeft":
@@ -104,6 +110,5 @@ document.addEventListener("keydown", (e) => {
         case "ArrowRight":
             player.moveRight();
             break;
-       }
+    }
 });
-
